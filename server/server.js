802402fc7b5ac1f599cbc52a088b6ee67a353632
teckjuec <teckjuec@student.unimelb.edu.au>
2022-09-07 16:27:16 +1000

    Connected server (nodejs) to database (mongodb)

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
@@ -7,4 +8,19 @@ app.use('/', require("./routes/home"));
 app.use('/', require("./routes/profile"));
 app.use('/', require("./routes/collection"));
   
+// Connect to the database
+mongoose.connect('mongodb+srv://josh:test123@cluster0.jvj0nic.mongodb.net/?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+  
+// Test if the connection was made
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
