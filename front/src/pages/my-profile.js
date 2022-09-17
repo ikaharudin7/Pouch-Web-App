1cde65e8144b12dc13de21b3fb0b4ebf072a9bd7
ikaharudin7 <ikaharudin7@gmail.com>
2022-09-17 22:18:11 +1000

    POST WORKS

--- front/src/pages/my-profile.js
+++ front/src/pages/my-profile.js
@@ -1,24 +1,83 @@
-import './my-profile.css'
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import '../App.css'; 
+import './my-profile.css';
 import ButtonBases from '../components/Button';
 import { Button } from '@mui/material';
-import { useState, useEffect } from 'react';
 
-  
-const Profile = () => {
-  const [state, setState] = useState(false);
 
-  useEffect(() => {
-    console.log('State is now: ', state);
-  }, [state]);
-
-  const editOn = event => {
-    setState(true);
+class Profile extends Component {
+state = {
+    data: null
   };
 
-  const editOff = event => {
-    setState(false);
+  handleChange = (event) => {
+    this.setState({[event.target.name]: event.target.value});
   };
+ 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    
+    fetch('http://localhost:8080/profile', {
+        method: "POST",
+        body: JSON.stringify({"title": "defaulttitle"})
+      }).then(()=> {
+        console.log("POST sent");
+      })
+ 
+  }; 
 
+  render() {
+    return (
+      <div>
+      <section className="hero">
+        <div className="hero-body">
+          <Button className="edit-button">EDIT PAGE</Button>
+          <div className="main">
+            <div className="container-right">
+              <h1 className="hero-title">About Me </h1>
+              <p className="hero-text">
+                BIO FOR USER GOES HERE
+              </p>
+              <br></br>
+
+              <form onSubmit={this.handleSubmit}>
+              <label>
+                  Name:
+                  <input type="text" value={this.state.value} name="name" onChange={this.handleChange} />
+              </label>
+              <input type="submit" value="Submit" />
+              </form>
+
+            </div>
+          </div>
+
+            <div className="featured-body">
+              <h1 className="featured-heading"> My Collections</h1>
+            </div>
+            <div className="panels-section">
+                <div className="panel">
+                <ButtonBases className="bottom-panels"/> 
+                </div>
+                <div className="bottom-panels">
+                <ButtonBases /> 
+                </div>
+                <div className="bottom-panels">
+                <ButtonBases /> 
+                </div>
+            </div>
+          </div>    
+      </section>
+    </div>
+
+      
+
+    );
+  }
+}
+
+export default Profile;
+/*
   return (
     <div>
       <section className="hero">
@@ -32,10 +91,14 @@ const Profile = () => {
               </p> : null}
               <br></br>
 
+<<<<<<< Updated upstream
               <form className="bio" action="/user/profile" method="post">
+=======
+              <form className="bio" onSubmit={this.handleSubmit}>
+>>>>>>> Stashed changes
                 { state ? <label type="text" className="input-text">Your bio here:</label> : null}
                 { state ? <br></br> : null}
-                { state ? <textarea className='bio-box' name="bio-section" type="text"> </textarea> : null}
+                { state ? <textarea className='bio-box' name="bio-section" type="submit" value="s"> </textarea> : null}
                 <br></br>
                 { state ? <Button className="edit-button" type="submit" onClick={editOff}>SAVE</Button> : null}
               </form>
@@ -63,4 +126,4 @@ const Profile = () => {
   );
 }
 
-export default Profile;
+export default Profile; */
--- server/routes/profile.js
+++ server/routes/profile.js
@@ -6,8 +6,14 @@ router.get('/profile', function (req, res) {
     res.send('This is the profile page');
 })
 
+<<<<<<< Updated upstream
 router.get('/profile', function (req, res) {
     res.send(pController.test);
+=======
+router.post('/', async(req, res)=>{
+    res.json({message: "backend post req success"})
+    console.log("Post WORKS");
+>>>>>>> Stashed changes
 })
 
 module.exports = router;
